2a780315894964923e03e09f393820837c53f85a
Юля Виноградова <juliavinooo@gmail.com>
2024-03-27 20:27:49 +0300

    module9-task1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { createPhotoCard } from './data.js';
 import { savePhotos } from './photo-state.js';
 import {renderThumbnails} from './thumbnails.js';
-
+import './form.js';
 const photos = createPhotoCard(25);
 savePhotos(photos);
 renderThumbnails(photos);
--- js/util.js
+++ js/util.js
@@ -8,6 +8,7 @@ const getRandomInteger = (a,b) => {
 const getRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
+const isUniqueArray = (array) => new Set(array).size === array.length;
 
-export {getRandomElement,getRandomInteger,isEscapeKey};
+export {getRandomElement,getRandomInteger,isEscapeKey,isUniqueArray};
 
