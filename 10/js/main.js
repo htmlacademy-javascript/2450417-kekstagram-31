c5f123bc9e45d6538ceec622c4218c0bee2f04f9
Keks <keks@htmlacademy.ru>
2024-03-27 20:33:06 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,9 @@
+
+import { createPhotoCard } from './data.js';
+import { savePhotos } from './photo-state.js';
+import {renderThumbnails} from './thumbnails.js';
+import './form.js';
+
+const photos = createPhotoCard(25);
+savePhotos(photos);
+renderThumbnails(photos);
---
+++ 10/js/util.js
@@ -0,0 +1,16 @@
+const getRandomInteger = (a,b) => {
+  const lower = Math.ceil (Math.min(a,b));
+  const upper = Math.floor (Math.max(a,b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const isUniqueArray = (array) => new Set(array).size === array.length;
+
+export {getRandomElement,getRandomInteger,isEscapeKey,isUniqueArray};
+
+
